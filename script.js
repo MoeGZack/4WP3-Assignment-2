4ab497f2054457f25ef53c5ac3826126ff841587
MoeGzack <muhammadameen335@gmail.com>
2026-02-23 13:44:04 -0500

    Trying to append container with results but having issues. Data isnt defined so potentially need to reduce the Json to retrieve only what I need and append its details to my Front end

--- script.js
+++ script.js
@@ -4,8 +4,10 @@ document .getElementById("search-form").addEventListener("submit", function(even
    
     const platform = document.getElementById("platform").value.trim();
     console.log("Minimum platform:", platform);
+
     const genre = document.getElementById("genre").value.trim();
     console.log("Genre:", genre);   
+
     const resultsamount = document.getElementById("resultsamount").value.trim();
     console.log("Results Amount:", resultsamount);
     
@@ -19,7 +21,27 @@ document .getElementById("search-form").addEventListener("submit", function(even
     })
         .then(response => response.json())
         .then(data => {
+            const resultsDiv = document.getElementById("results");
+            resultsDiv.innerHTML = "";
             console.log("Server response:", data);
         })
+        data.results.forEach(game => {
+            
+            const container= document.createElement("div");
+            const title = document.createElement("h3");
+            title.textContent = game.name;
+
+            const image = document.createElement("img");
+            image.src = document.createElement("img");
+            image.src = game.background_image;
+            image.width = 200;
+
+            container.appendChild(title);
+            container.appendChild(image);
+            
+            resultsDiv.appendChild(container);
+
+
+        });
     })
         
--- server.js
+++ server.js
@@ -19,26 +19,26 @@ app.get("/app", function(req, res) {
 });
 
 
-//app.post("/search", function(req, res) {
-//const {searchname, genre, platform, resultsamount} = req.body;
-//console.log("Received search parameters:", searchname, genre, platform, resultsamount);
-//res.json({ message: "Search parameters received", searchname, genre, platform, resultsamount });
-//});
 
 app.post("/search", function(req, res) {
   const {genre, platform, resultsamount } = req.body;
   console.log("Received search parameters:", genre, platform, resultsamount);
 
   const rawAPIresponse=  fetch(`${apiUrl}?key=${apiKey}&genres=${encodeURIComponent(genre)}&platforms=${encodeURIComponent(platform)}&page_size=${encodeURIComponent(resultsamount)}`)
+
     .then(response => response.json())
+
     .then(data => {
       console.log("API response data:", data);
       res.json(data);
     })
+
     .catch(error => {
       console.error("Error fetching API data:", error);
       res.status(500).json({ error: "Failed to fetch API data" });
+
     });
+
 });
 
 const server = app.listen(port, hostname, function() {
