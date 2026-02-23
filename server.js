6887b50cd08f5abd0dbeb3dc466f876887c4f735
MoeGzack <muhammadameen335@gmail.com>
2026-02-23 10:22:46 -0500

    got styles.css linked. I didnt have static express. I could have done app.get for each file but that would have been messy. Got this from W3schools

--- server.js
+++ server.js
@@ -2,16 +2,15 @@ const fs = require("fs");
 const express = require("express");
 
 const app = express();
-
+app.use(express.static(__dirname));
 
 app.use(express.urlencoded({ extended: true })); 
 
 
+app.get("/", function(req, res) {
 
-app.get("/app", function(req,res) {
-  res.sendFile(`${__dirname}/index.html`);
+  res.sendFile(__dirname + "/index.html");
 });
-
 
 const server = app.listen(3000, function() {
 
