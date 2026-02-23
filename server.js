86a0a3e2318807a319070c729cd4cdd2850eecd4
MoeGzack <muhammadameen335@gmail.com>
2026-02-23 10:43:49 -0500

    added constant port and local host as hostname for the server

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const fs = require("fs");
 const express = require("express");
+const http = require("http");
+const port = 3000;
+const hostname = "localhost";
 
 const app = express();
 app.use(express.static(__dirname));
@@ -12,8 +15,8 @@ app.get("/app", function(req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
-const server = app.listen(3000, function() {
+const server = app.listen(port, hostname, function() {
 
-  console.log(`Listening for requests`);
+  console.log(`Listening for requests on http://${hostname}:${port}`);
 });
 
